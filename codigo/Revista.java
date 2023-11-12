8cccf42476eab7eb44bc916cc2111bddad6d6658
luccaovf <lovfaria@sga.pucminas.br>
2023-11-12 19:03:30 -0300

    deletando duplicado

--- codigo/Revista.java
+++
@@ -1,6 +0,0 @@
-public class Revista extends ItemBibli{
-    // Construtor que chama o construtor da classe mãe
-    public Revista(String autor, String titulo, int ano, int exemplares) {
-        super(autor, titulo, ano, exemplares);
-    }
-}
--- codigo/Tese.java
+++
@@ -1,6 +0,0 @@
-public class Tese extends ItemBibli{
-    // Construtor que chama o construtor da classe mãe
-    public Tese(String autor, String titulo, int ano, int exemplares) {
-        super(autor, titulo, ano, exemplares);
-    }
-}
